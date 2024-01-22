b7a4b10431018abf777d39e0e1b0ee3d8ccbed49
dominic-myb <desguerra246@gmail.com>
2024-01-22 17:06:55 +0800

    Added Include Files

---
+++ app/includes/html/html.head.php
@@ -0,0 +1,7 @@
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="assets/css/vendor/bootstrap-5.2.3-dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="<?php echo $styleCSS;?>">
+    <title><?php echo $pageTitle; ?></title>
+</head>
---
+++ app/includes/html/html.scripts.php
@@ -0,0 +1,2 @@
+<script src="assets/js/vendor/jquery-3.7.1.min.js"></script>
+<script src="assets/js/scripts.js"></script>
