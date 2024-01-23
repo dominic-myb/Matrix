3c636a07d0fd4dfbebebb9d07350ab0fe1844bfc
dominic-myb <desguerra246@gmail.com>
2024-01-23 11:25:01 +0800

    Changed DB sample data

--- password_db.sql
+++ password_db.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jan 16, 2024 at 03:57 AM
--- Server version: 10.4.28-MariaDB
--- PHP Version: 8.0.28
+-- Generation Time: Jan 23, 2024 at 04:16 AM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -36,6 +36,18 @@ CREATE TABLE `user_tbl` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
+-- Dumping data for table `user_tbl`
+--
+
+INSERT INTO `user_tbl` (`id`, `username`, `password`, `encrypted_pass`, `department`) VALUES
+(91, 'dominic', '1234', '03ac674216f3e15c761ee1a5e255f067953623c8b388b4459e13f978d7c846f4', 'DCS'),
+(92, 'ailes', 'sped', '96466cddbbf9e52fe585ecb0fa3f9dd16c7f92c656d8e958df8ad9fff9f7a44b', 'DTE'),
+(93, 'culanding', 'oksjdiksjd', 'ce564c3998163aee2a16abfbfb21e6a1a0306952f471c9f853e5570c0746a3ca', 'DEE'),
+(94, 'andrei', 'asdkjaskjdbasd', '9f0b1c3a929f86afb70796cd33fbb54eda1d3eed941d6c9df4719d0302a6657e', 'DCS'),
+(95, 'jack', 'asdasdasdasdasdfdgdfg', 'ad980ef9ac3aa28aec6a8fe705e5e49d3c955cc618bde024d56632bc503da5d4', 'DTE'),
+(96, 'darren', 'bigorns', '3b134677f00b46c0e45ffdabe270eea99d526d599fb52cba6c5663f69f34e665', 'DEE');
+
+--
 -- Indexes for dumped tables
 --
 
@@ -53,7 +65,7 @@ ALTER TABLE `user_tbl`
 -- AUTO_INCREMENT for table `user_tbl`
 --
 ALTER TABLE `user_tbl`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=97;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
